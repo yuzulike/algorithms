8406d4e945b06e16cd6a6240735a6cf7662c2be4
Yuju <108673988+yuzulike@users.noreply.github.com>
2023-07-21 14:42:02 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -0,0 +1,4 @@
+function solution(my_string, overwrite_string, s) {
+    var answer = my_string.substring(0,s) + overwrite_string + my_string.substring(s+overwrite_string.length);
+    return answer;
+}
