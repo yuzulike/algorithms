2e910aff6fe5636ef1cc0b1da3a1a4f50d116f7a
Yuju <108673988+yuzulike@users.noreply.github.com>
2023-07-24 11:50:09 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,4 +1,5 @@
 function solution(arr) {
-    let newArr= arr.filter(ele => ele > Math.min(...arr))
+    const min = Math.min(...arr)
+    let newArr= arr.filter(ele => ele > min)
     return  newArr.length ? newArr : [-1];
 }
