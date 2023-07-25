b701aacee83dca78de461992e5f8b9c9b2cb94e7
Yuju <108673988+yuzulike@users.noreply.github.com>
2023-07-25 10:55:54 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.20 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,8 +1,8 @@
 function solution(t, p) {
     var answer = 0;
-    for(let i=0 ; i<t.length - p.length + 1 ; i++){
-       let number =t.slice(i,i+p.length)
-       if(number <= p) answer += 1;
+    for(let i=0 ; i<t.length - p.length + 1 ; i++){ // p의 길이로 잘랐을 때 나오는 갯수
+       let number =t.slice(i,i+p.length) // p의 길이와 똑같게 자름
+       if(number <= p) answer += 1; // p보다 작을 시 answer 횟수 추가
     }
     return answer;
 }
