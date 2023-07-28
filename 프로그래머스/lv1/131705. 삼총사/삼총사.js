cdb0f7f7314eb5f130e371bde363bdedd68e56a3
Yuju <108673988+yuzulike@users.noreply.github.com>
2023-07-28 11:24:37 +0900

    [level 1] Title: 삼총사, Time: 2.34 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -0,0 +1,13 @@
+function solution(number) {
+    var answer = 0;
+    let added = 0;
+    for (let i =0; i< number.length ; i++){
+        for(let j=i+1; j<number.length; j++ ){
+        added = number[i]+number[j];
+        let newNum= number.filter((ele,n)=> ele== -added && n>j)   
+            if(newNum.length) answer += newNum.length ;
+        }
+    }
+    console.log(answer)
+    return answer;
+}
