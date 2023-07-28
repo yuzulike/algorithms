7e9030263a7b0559c30c2c3f1638a79e73c0fa61
Yuju <108673988+yuzulike@users.noreply.github.com>
2023-07-28 10:01:46 +0900

    [level 1] Title: 내적, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.js
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -1,7 +1,12 @@
+// function solution(a, b) {
+//     var answer = 0;
+//     for(let i=0; i<a.length; i++){
+//      answer += a[i]*b[i]
+//     }
+//     return answer;
+// }
+
 function solution(a, b) {
-    var answer = 0;
-    for(let i=0; i<a.length; i++){
-     answer += a[i]*b[i]
-    }
-    return answer;
+    return a.reduce((acc,cur,i)=> acc + cur*b[i],0)
+    
 }
