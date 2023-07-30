451a489cd7ac955406cec2819a1a25d940058f33
Yuju <108673988+yuzulike@users.noreply.github.com>
2023-07-30 22:59:56 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.34 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,15 +1,12 @@
 function solution(s) {
-    var answer = [];
-    let arr = s.split(' ');
-     let mapFunction= ele =>{
-         let newEle = ''
-        for(let i=0 ; i<ele.length ; i++){
-        if(i % 2) newEle += ele[i].toLowerCase();
-        else newEle += ele[i].toUpperCase() ;
-        }
-            return newEle;
+    let arr = s.split(' '); // 띄어쓰기 기준으로 배열에 담기
+     let mapFunction= ele =>{ // map() 콜백함수
+         let newEle = '' 
+            for(let i=0 ; i<ele.length ; i++){ 
+            if(i % 2) newEle += ele[i].toLowerCase(); // 홀수번째는 소문자로
+            else newEle += ele[i].toUpperCase() ; // 짝수번째는 대문자로
+            }
+        return newEle;
     }
-    let newArr= arr.map(ele=> mapFunction(ele))
-   
-    return newArr.join(' ');
+    return arr.map(ele=> mapFunction(ele)).join(' '); // map 돌린 후 다시 문자열로 join
 }
