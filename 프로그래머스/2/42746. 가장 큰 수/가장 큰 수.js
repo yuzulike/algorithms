31fb388d55c0bb186409165be889274d8d5bff08
Yuju <108673988+yuzulike@users.noreply.github.com>
2025-01-11 09:26:01 +0900

    [level 2] Title: 가장 큰 수, Time: 162.49 ms, Memory: 45 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,12 +1,6 @@
 function solution(numbers) {
-    // 숫자를 문자열로 변환 후 정렬
-    let sortedArr = numbers.map(String).sort((a, b) => (b + a) - (a + b));
 
-    // 정렬된 결과가 모두 0일 경우 처리
-    if (sortedArr[0] === "0") {
-        return "0";
-    }
-
-    // 정렬된 배열을 문자열로 합치기
-    return sortedArr.join('');
+    const result=
+    numbers.map(item=>String(item)).sort((a,b)=>(b+a)-(a+b)).join('');
+    return result[0]==="0"? "0":result
 }
